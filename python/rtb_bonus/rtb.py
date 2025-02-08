ef5f16b85d92f18bb0f4271aac4383d2d41b77d3
兆里 <zhuansunpengcheng@cai-inc.com>
2025-02-08 17:30:50 +0800

    fix

--- python/rtb_bonus/rtb.py
+++ python/rtb_bonus/rtb.py
@@ -11,51 +11,46 @@ class DataObject:
         self.vipMoney = vipMoney
         self.source = source
 
+    def to_dict(self):
+        return {
+            "dataStr": self.dataStr,
+            "mobileCount": self.mobileCount,
+            "mobleStore": self.mobleStore,
+            "vipMoney": self.vipMoney,
+            "source": self.source
+        }
+
     def __repr__(self):
         return f"DataObject(dataStr={self.dataStr}, mobileCount={self.mobileCount}, mobleStore={self.mobleStore}, vipMoney={self.vipMoney}, source={self.source})"
 
+
 def method1(data):
-    # 创建一个空列表来存储解析后的对象
     result_objects = []
-    
-    # 将字符串转换为字典
     data_dict = json.loads(data)
-    
-    # 遍历data中的list部分
     for item in data_dict['data']['list']:
-        # 解析对象
         data_object = DataObject(
             dataStr=item['date_str'],
-            mobileCount=item['u_count1'],
-            mobleStore=item['u_count2'],
-            vipMoney=item['u_money3'],
+            mobileCount=int(item['u_count1']),
+            mobleStore=int(item['u_count2']),
+            vipMoney=float(item['u_money3']),
             source='BAIDU'
         )
-        # 将解析后的对象添加到列表中
         result_objects.append(data_object)
-    
     return result_objects
 
+
 def method2(data):
-    # 创建一个空列表来存储解析后的对象
     result_objects = []
-    
-    # 将字符串转换为字典
     data_dict = json.loads(data)
-    
-    # 遍历data中的list部分
     for item in data_dict['data']['list']:
-        # 解析对象
         data_object = DataObject(
             dataStr=item['date_str'],
-            mobileCount=item['mobile_count'],
-            mobleStore=item['store_count'],
-            vipMoney=item['mobile_fencheng'],
+            mobileCount=int(item['mobile_count']),
+            mobleStore=int(item['store_count']),
+            vipMoney=float(item['mobile_fencheng']),
             source='QUARK'
         )
-        # 将解析后的对象添加到列表中
         result_objects.append(data_object)
-    
     return result_objects
 
 
@@ -64,63 +59,75 @@ baiduUrl = 'https://dt.bd.cn/main/baidupan_list'
 quarkUrl = 'https://dt.bd.cn/main/quark_list'
 
 bsList = [
-    {"type":"BAIDU","bs":"4236298918"},
-    {"type":"QUARK","bs":"100187470341"},
-    {"type":"QUARK","bs":"100676748675"}
+    {"type": "BAIDU", "bs": "4236298918"},
+    {"type": "QUARK-1", "bs": "100187470341", "auth_key": "7c55ttOvyAu8n/VzfoxLrY3PFTB/IyC9BJjcal2+iUc9igYdPtrSlA"},
+    {"type": "QUARK-2", "bs": "100676748675", "auth_key": "7c55ttOvyAu8n/VzfoxLrY3PFTB/IyC9BJjcal2+iUc9igYdPtrSlA"}
 ]
 
-# 请求头
 headers = {
     'X-Requested-With': 'XMLHttpRequest',
     'Content-Type': 'application/x-www-form-urlencoded'
 }
 
-# Cookie
 cookies = {
-    'PHPSESSID': 'v9g20g7qema86f86d6t4md5gh0',
-    'SERVERID': '60db755fec3430d627f0ed585eca91ed|1734521355|1734521355',
-    'acw_tc': '0b32807e17345213555523488e53eb29ae1ab7e19bfaa42afbe9dbc2142f59'
+    'PHPSESSID': '37kvg1schqbmbu3barc1ohpau8',
+    'SERVERID': '60db755fec3430d627f0ed585eca91ed|1738719331|1738719330',
+    'acw_tc': '0b32807e17387193299811095e78361bf4324f0ce9a7e5d8b6afd282b0f7f2'
 }
 
-# 获取当前时间
 current_date = datetime.now()
-
-# 获取当前时间的前20天
-start_date = current_date - timedelta(days=20)
-
-# 格式化日期为 YYYY-MM-DD
+start_date = current_date - timedelta(days=10)
 formatted_start_date = start_date.strftime('%Y-%m-%d')
 formatted_end_date = current_date.strftime('%Y-%m-%d')
 
-# POST 请求的数据模板
 data_template = {
     'start_date': formatted_start_date,
     'end_date': formatted_end_date
 }
 
 result_list = []
+quark_data = []
 
-# 遍历 bsList
 for item in bsList:
-    # 根据 type 设置 data 中的 bs
-    # 复制数据模板
     data = data_template.copy()
     data['bs'] = item["bs"]
-
     try:
         if item["type"] == "BAIDU":
-            # 发起 POST 请求
             response = requests.post(baiduUrl, headers=headers, cookies=cookies, data=data)
-            # 调用 method1 处理 response.text，结果放进result_list中
             result_list.extend(method1(response.text))
-        elif item["type"] == "QUARK":
-            # 发起 POST 请求
+        elif item["type"].startswith("QUARK"):
+            data['auth_key'] = item["auth_key"]
             response = requests.post(quarkUrl, headers=headers, cookies=cookies, data=data)
-            # 调用 method2 处理 response.text，结果放进result_list中
-            result_list.extend(method2(response.text))
+            quark_data.extend(method2(response.text))
     except requests.RequestException as e:
         print(f"请求错误: {e}")
 
-# 打印结果
-for result in result_list:
-    print(result)
+# 合并QUARK数据
+merged_quark = {}
+for obj in quark_data:
+    key = obj.dataStr
+    if key not in merged_quark:
+        merged_quark[key] = {
+            'mobileCount': 0,
+            'mobleStore': 0,
+            'vipMoney': 0.0,
+            'source': 'QUARK'
+        }
+    merged_quark[key]['mobileCount'] += obj.mobileCount
+    merged_quark[key]['mobleStore'] += obj.mobleStore
+    merged_quark[key]['vipMoney'] += obj.vipMoney
+
+# 添加合并后的QUARK数据到结果列表
+for date_str, data in merged_quark.items():
+    data_obj = DataObject(
+        dataStr=date_str,
+        mobileCount=data['mobileCount'],
+        mobleStore=data['mobleStore'],
+        vipMoney=data['vipMoney'],
+        source=data['source']
+    )
+    result_list.append(data_obj)
+
+# 将 result_list 转换为 JSON 字符串并打印
+json_result = json.dumps([obj.to_dict() for obj in result_list], ensure_ascii=False)
+print(json_result)
